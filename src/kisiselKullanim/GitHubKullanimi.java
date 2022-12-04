5cd1d11865890d91e533e4e13d1960b912c77ca9
Asya ilhanli <r.asyailhanli@gmail.com>
2022-12-04 08:39:07 -0800

    ilk degisiklik

--- src/kisiselKullanim/GitHubKullanimi.java
+++ src/kisiselKullanim/GitHubKullanimi.java
@@ -3,6 +3,8 @@ package kisiselKullanim;
 public class GitHubKullanimi {
     public static void main(String[] args) {
 
-        System.out.println("Hello Word");
+        System.out.println("Hello World");
+
+        System.out.println("That's it ");
     }
 }
